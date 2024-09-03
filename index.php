3abe1ea44de5474b90e83d5956864659e13b3486
antonio909 <78562889+antonio909@users.noreply.github.com>
2024-09-03 19:11:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
+<html>
+  
+  <head>
+    
+    <title>Site em PHP</title>
 
+    <link rel="stylesheet" type="text/css" href="css/formato.css">
+    
+  </head>
+  
+  <body>
+    
+    <div id="topo">Topo</div>
+    
+    <div id="menu">Menu</div>
+    
+    <div id="conteudo">Conteúdo</div>
+    
+    <div id="rodape">Rodapé</div>
+    
+  </body>
+  
+</html>
